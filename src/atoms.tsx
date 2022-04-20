3c173b7627a20fc620f97393a83591f9a54549f1
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-04-20 18:54:00 +0900

    서로다른 구역으로 state 이동하기

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,15 +1,14 @@
 import { atom, selector } from "recoil";
 
-export const toDoState = atom({
+interface IToDoState {
+  [key: string]: string[];
+}
+
+export const toDoState = atom<IToDoState>({
   key: "toDo",
-  default: [
-    "먹기",
-    "운동하기",
-    "청소하기",
-    "강아지산책하기",
-    "헬스하기",
-    "샤워하기",
-    "잠자기",
-    "공부하기",
-  ],
+  default: {
+    todo: ["운동하기", "강아지산책하기", "헬스하기", "샤워하기", "잠자기"],
+    doing: ["공부하기"],
+    done: ["청소하기"],
+  },
 });
