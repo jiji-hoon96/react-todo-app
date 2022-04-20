703659bd7ad9f34d1fc28c498c6741374489efeb
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-04-20 15:13:00 +0900

    recoil의 atom과 selector 연습
    set,get을 이용해 값을 변경하는 방법 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
+
 function App() {
-  return <div></div>;
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+  };
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="분"
+      />
+      <input
+        value={hours}
+        onChange={onHoursChange}
+        type="number"
+        placeholder="시간"
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,1 +1,18 @@
 import { atom, selector } from "recoil";
+
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
+});
+
+export const hourSelector = selector<number>({
+  key: "hours",
+  get: ({ get }) => {
+    const hours = get(minuteState);
+    return hours / 60;
+  },
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes);
+  },
+});
