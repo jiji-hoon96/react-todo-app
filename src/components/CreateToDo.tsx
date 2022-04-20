d59e42b2336cb144c15533d15d635f9a5c65a516
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-04-20 13:37:19 +0900

    3. toDo state 구현

---
+++ src/components/CreateToDo.tsx
@@ -0,0 +1,39 @@
+import { useForm } from "react-hook-form";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { categoryState, toDoState } from "../atoms";
+
+interface IForm {
+  toDo: string;
+}
+
+function CreateToDo() {
+  const setToDos = useSetRecoilState(toDoState);
+  const category = useRecoilValue(categoryState);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm<IForm>();
+  const handleValid = ({ toDo }: IForm) => {
+    setToDos((oldToDos) => [
+      { text: toDo, id: Date.now(), category },
+      ...oldToDos,
+    ]);
+    setValue("toDo", "");
+  };
+  return (
+    <form onSubmit={handleSubmit(handleValid)}>
+      <input
+        {...register("toDo", {
+          required: "반드시 ToDo를 입력해주세요",
+        })}
+        placeholder="Write a to do"
+      />
+      <span>{errors?.toDo?.message}</span>
+      <button>Add</button>
+    </form>
+  );
+}
+
+export default CreateToDo;
